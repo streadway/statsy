3ddbe0b07dd510131ab75e2f126f301d3f2e3665
Sean Treadway <sean@soundcloud.com>
2011-09-27 20:43:37 +0200

    Bump version

--- lib/statsy.rb
+++ lib/statsy.rb
@@ -1,7 +1,7 @@
 # Client to access statsd service authored by etsy. Yay etsy!
 # https://github.com/etsy/statsd
 module Statsy
-  VERSION="0.1.1"
+  VERSION="0.1.2"
 
   module Transport
     require 'socket'
