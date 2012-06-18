c4ebbfb77412b955f26d520622529c0e17e4a197
Tobias Schmidt <ts@soundcloud.com>
2012-06-18 18:00:56 +0200

    Release v0.2.0

--- lib/statsy.rb
+++ lib/statsy.rb
@@ -1,7 +1,7 @@
 # Client to access statsd service authored by etsy. Yay etsy!
 # https://github.com/etsy/statsd
 module Statsy
-  VERSION="0.1.3"
+  VERSION='0.2.0'
 
   module Transport
     require 'socket'
